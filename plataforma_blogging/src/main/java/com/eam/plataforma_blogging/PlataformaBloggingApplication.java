44b94896be39a98973c6e80e5d74e5caa1561362
sebastiancruzcardona <sebastiancruzcardona90@gmail.com>
2024-08-24 20:31:38 -0500

    Add blogging platform project created with spring initializr

---
+++ plataforma_blogging/src/main/java/com/eam/plataforma_blogging/PlataformaBloggingApplication.java
@@ -0,0 +1,13 @@
+package com.eam.plataforma_blogging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaBloggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlataformaBloggingApplication.class, args);
+	}
+
+}
---
+++ plataforma_blogging/src/test/java/com/eam/plataforma_blogging/PlataformaBloggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eam.plataforma_blogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlataformaBloggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
