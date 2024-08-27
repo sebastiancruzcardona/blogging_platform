e86f865a0e33f5801c1d9c4c4c75c91f6534cd8b
Andres <arperdomo26@gmail.com>
2024-08-26 23:11:11 -0500

    feat(CategoriesPost): Create class

---
+++ blogging_platform/src/main/java/com/eam/blogging_platform/entity/CategoriesPost.java
@@ -0,0 +1,55 @@
+package com.eam.blogging_platform.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "categories_post")
+public class CategoriesPost {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    public CategoriesPost() {
+        super();
+    }
+
+    public CategoriesPost(int id, Post post, Category category) {
+        this.id = id;
+        this.post = post;
+        this.category = category;
+    }
+
+    public CategoriesPost(Post post, Category category) {
+        this.post = post;
+        this.category = category;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
+
