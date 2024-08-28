bd8c859e9250be826af992b0cc301f8e2ab871dc
sebastiancruzcardona <sebastiancruzcardona90@gmail.com>
2024-08-28 16:23:39 -0500

    Fix(Post): Add OneToMany tag to connect with CategoriesPost

--- blogging_platform/src/main/java/com/eam/blogging_platform/entity/Post.java
+++ blogging_platform/src/main/java/com/eam/blogging_platform/entity/Post.java
@@ -48,6 +48,9 @@ public class Post {
     @OneToMany(mappedBy = "post")
     private List<Comment> comments;
 
+    @OneToMany(mappedBy = "post")
+    private List<CategoriesPost> categoriesPosts;
+
     public Post() {
         super();
     }
