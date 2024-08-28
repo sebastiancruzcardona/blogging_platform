f5024a7b8e109f7a6dd5ecc6f9ae6f191bb9ffab
sebastiancruzcardona <sebastiancruzcardona90@gmail.com>
2024-08-28 16:02:50 -0500

    Fix(User): Add OneToMany tag to connect with Post.user

--- blogging_platform/src/main/java/com/eam/blogging_platform/entity/User.java
+++ blogging_platform/src/main/java/com/eam/blogging_platform/entity/User.java
@@ -39,6 +39,9 @@ public class User {
     @OneToMany(mappedBy = "user")
     private List<Comment> comments;
 
+    @OneToMany(mappedBy = "user")
+    private List<Post> posts;
+
     public User() {
         super();
     }
