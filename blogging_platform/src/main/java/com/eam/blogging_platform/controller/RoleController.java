f2a151c775f9c426fa9d8fee7542e13cfde42777
sebastiancruzcardona <sebastiancruzcardona90@gmail.com>
2024-10-09 22:00:50 -0500

    Refactor(RoleController, RoleService): Update methods for using RoleDTO and RoleDTOGetPostPut

--- blogging_platform/src/main/java/com/eam/blogging_platform/controller/RoleController.java
+++ blogging_platform/src/main/java/com/eam/blogging_platform/controller/RoleController.java
@@ -1,7 +1,10 @@
 package com.eam.blogging_platform.controller;
 
+import com.eam.blogging_platform.dto.RoleDTO;
+import com.eam.blogging_platform.dto.RoleDTOGetPostPut;
 import com.eam.blogging_platform.entity.Role;
 import com.eam.blogging_platform.service.RoleService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,50 +18,49 @@ public class RoleController {
     @Autowired //Singleton backwards for just one RoleService instance
     private RoleService roleService;
 
-    //This method refers to roleService.findAll() method. Brings out every RoleDTO stored in database table role as a List of roles
+    //This method refers to roleService.findAll() method. Brings out every role stored in database table role as a List of RoleDTOs
     @GetMapping
-    public List<Role> getAllRoles() {
+    public List<RoleDTOGetPostPut> getAllRoles() {
         return roleService.findAll();
     }
 
-    //This method refers to roleService.findById() method. Finds a specific role searching by id
-    //If the role is found, maps the ResponseEntity and returns a 200 OK Status.
-    //If there is not a role identified by that id, returns 404 Not Found Status
     @GetMapping("/{id}")
-    public ResponseEntity<Role> getRoleById(@PathVariable long id){
-        Optional<Role> role = roleService.findById(id);
-        return role.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    //This method calls the findById method from roleService that returns an Optional
+    //Then, tries to map the Optional RoleDTOGetPostPut by using the .ok() function from ResponseEntity, for this the role has to be present
+    //If the optional is empty, executes the orElseGet() implementing a ResponseEntity.notFound().build()
+    //It is equivalent to writing:
+        /*if(bankDTOGetPutPost.isPresent()){
+          return ResponseEntity.ok(bankDTOGetPutPost);
+        else{
+          return ResponseEntity.notFound().build();
+        }*/
+    public ResponseEntity<RoleDTOGetPostPut> getBankById(@PathVariable long id){
+        Optional<RoleDTOGetPostPut> roleDTOGetPutPost = roleService.findById(id);
+        return roleDTOGetPutPost.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     //This method refers to roleService.save() method. Saves a new role in database table role
     @PostMapping
-    public Role createRole(@RequestBody Role role){
-        return roleService.save(role);
+    public RoleDTOGetPostPut createBank(@Valid @RequestBody RoleDTO roleDTO){
+        return roleService.save(roleDTO);
+    }
+    
+    @PutMapping("/{id}")
+    //This method calls the update method from roleService that needs an id and a RoleDTO object and returns an Optional
+    //Then, tries to map the Optional RoleDTOGetPostPut by using the .ok() function from ResponseEntity, for this the roleDTOGetPostPut has to be present
+    //If the optional is empty, executes the orElseGet() implementing a ResponseEntity.notFound().build()
+    public ResponseEntity<RoleDTOGetPostPut> updateBank(@PathVariable long id, @Valid @RequestBody RoleDTO roleDTO){
+        Optional<RoleDTOGetPostPut> roleDTOGetPutPost = roleService.update(id, roleDTO);
+        return roleDTOGetPutPost.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    //This method refers to roleService.findById() and roleService.save() methods. Finds a specific role searching by id and updates it
-    //If the role is found, sets the attributes to the role in edition, saves to update and returns a 200 OK Status.
-    //If there is not a role identified by that id, returns 404 Not Found Status
-    @PutMapping("/{id}")
-    public ResponseEntity<Role> updateRole(@PathVariable long id, @RequestBody Role roleDetails){
-        Optional<Role> role = roleService.findById(id);
-        if(role.isPresent()){
-            Role updatedRole = role.get();
-            updatedRole.setRole(roleDetails.getRole());
-            updatedRole.setDescription(roleDetails.getDescription());
-            return ResponseEntity.ok(roleService.save(updatedRole));
-        }else{
-            return ResponseEntity.notFound().build();
-        }
-    }
 
     //This method refers to roleService.findById() and roleService.deleteById() methods. Finds a specific role searching by id and deletes it
     //If the role is found, deletes it.
     //If there is not a role identified by that id, returns 404 Not Found Status
     @DeleteMapping("{id}")
-    public ResponseEntity<Void> deleteRole(@PathVariable long id){
-        if(roleService.findById(id).isPresent()){
-            roleService.deleteById(id);
+    public ResponseEntity<Role> deleteBank(@PathVariable long id){
+        if(roleService.deleteById(id)){
             return ResponseEntity.ok().build();
         }else{
             return ResponseEntity.notFound().build();
--- blogging_platform/src/main/java/com/eam/blogging_platform/service/RoleService.java
+++ blogging_platform/src/main/java/com/eam/blogging_platform/service/RoleService.java
@@ -1,10 +1,13 @@
 package com.eam.blogging_platform.service;
 
+import com.eam.blogging_platform.dto.RoleDTO;
+import com.eam.blogging_platform.dto.RoleDTOGetPostPut;
 import com.eam.blogging_platform.entity.Role;
 import com.eam.blogging_platform.repository.RoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,23 +16,66 @@ public class RoleService {
     @Autowired //Singleton backwards for just one RoleRepository instance
     private RoleRepository roleRepository;
 
-    //This method brings out every RoleDTO stored in table role in database
-    public List<Role> findAll() {
-        return roleRepository.findAll();
+    //This method finds all roles stored in database and returns a list of RoleDTOGetPostPut
+    //Calls roleRepository.findAll() and uses a for cycle to iterate over the roles and to add to the Arraylist to return
+    public List<RoleDTOGetPostPut> findAll() {
+        List<RoleDTOGetPostPut> rolesToReturn = new ArrayList<>();
+        List<Role> roles = roleRepository.findAll();
+        for (Role role : roles) {
+            RoleDTOGetPostPut roleDTOGetPostPut = new RoleDTOGetPostPut();
+            roleDTOGetPostPut.convertToRoleDTO(role);
+            rolesToReturn.add(roleDTOGetPostPut);
+        }
+        return rolesToReturn;
     }
 
-    //This method finds a specific role searching by id
-    public Optional<Role> findById(long id) {
-        return roleRepository.findById(id);
+    //This method returns an Optional of RoleDTOGetPostPut
+    //Using id, if the searched role exist, returns the optional, if not, returns an empty Optional
+    public Optional<RoleDTOGetPostPut> findById(long id) {
+        Optional<Role> role = roleRepository.findById(id);
+        if(role.isPresent()) {
+            RoleDTOGetPostPut roleDTOGetPutPost = new RoleDTOGetPostPut();
+            roleDTOGetPutPost.convertToRoleDTO(role.get());
+            return Optional.of(roleDTOGetPutPost);
+        }
+        return Optional.empty();
     }
 
-    //This method saves a new role in database table role
-    public Role save(Role role){
-        return roleRepository.save(role);
+    //This method returns RoleDTOGetPostPut object
+    //Creates a role object, sets its attributes from RoleDTOGetPostPut received as parameter and saves it by calling roleRepository.save()
+    //Uses that Role as an assistant to save calling the repository save() function
+    public RoleDTOGetPostPut save(RoleDTO roleDTO) {
+        Role role = new Role();
+        role.setRole(roleDTO.getRole());
+        role.setDescription(roleDTO.getDescription());
+        RoleDTOGetPostPut roleDTOGetPostPut = new RoleDTOGetPostPut();
+        roleDTOGetPostPut.convertToRoleDTO(roleRepository.save(role));
+        return roleDTOGetPostPut;
     }
 
-    //This method deletes a specific role by using its id
-    public void deleteById(long id){
-        roleRepository.deleteById(id);
+    //This method returns an Optional that can be present or empty.
+    //First, it tries to find the Role by id, then, if the Optional role is present, sets the attributes and returns an Optional
+    //If there is not a role identified by that id, returns an empty optional
+    public Optional<RoleDTOGetPostPut> update(long id, RoleDTO roleDTO){
+        Optional<Role> role = roleRepository.findById(id);
+        if(role.isPresent()){
+            Role updatedrole = role.get();
+            updatedrole.setRole(roleDTO.getRole());
+            updatedrole.setDescription(roleDTO.getDescription());
+            RoleDTOGetPostPut roleDTOGetPostPut = new RoleDTOGetPostPut();
+            roleDTOGetPostPut.convertToRoleDTO(roleRepository.save(updatedrole));
+            return Optional.of(roleDTOGetPostPut);
+        }else{
+            return Optional.empty();
+        }
+    }
+
+    //This method, validating the Optional in the if block, returns true if deletion was made or false if not
+    public boolean deleteById(long id) {
+        if(roleRepository.findById(id).isPresent()) {
+            roleRepository.deleteById(id);
+            return true;
+        }
+        return false;
     }
 }
