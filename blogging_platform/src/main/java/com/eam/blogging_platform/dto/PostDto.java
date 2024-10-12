efe52cecf9a79e6847094ac6a7dfad8bf5242c18
Andres <arperdomo26@gmail.com>
2024-10-12 16:37:13 -0500

    feat(PostDTO): is modify the PostDTO add the validation

--- blogging_platform/src/main/java/com/eam/blogging_platform/dto/PostDto.java
+++ blogging_platform/src/main/java/com/eam/blogging_platform/dto/PostDto.java
@@ -1,38 +1,113 @@
 package com.eam.blogging_platform.dto;
 
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
+
+import java.time.LocalDateTime;
 
 public class PostDto {
 
-    @Min(1)
-    private Long user_id;
-
-    @NotBlank(message = "A post must be provided")
-    @Size(min = 2, max = 150, message = "Not a valid post")
+    @NotBlank(message = "A title must be provided")
+    @Size(min = 1, max = 150, message = "Not a valid title, 1 character as minimum, 150 as maximum")
     private String title;
 
-    @NotBlank(message = "A post must be provided")
-    @Size(min = 2, max = 100, message = "Not a valid post")
+    @NotBlank(message = "Content must be provided")
+    @Size(min = 1, max = 6000, message = "Not valid content, 1 character as minimum, 6000 as maximum")
     private String content;
 
-    @Min(1)
-    private Long status_id;
+    @NotNull(message = "User ID must be provided")
+    @Min(value = 1, message = "User ID must be greater than or equal to 1")
+    private Long userId;
 
-    public Long getUser_id() {return user_id;}
+    @NotNull(message = "Status ID must be provided")
+    @Min(value = 1, message = "Status ID must be greater than or equal to 1")
+    private Long statusId;
 
-    public void setUser_id(Long user_id) {this.user_id = user_id;}
+    @Min(value = 0, message = "Likes cannot be negative")
+    private int likes;
 
-    public String getTitle() {return title;}
+    @Min(value = 0, message = "Dislikes cannot be negative")
+    private int dislikes;
 
-    public void setTitle(String title) {this.title = title;}
+    @PastOrPresent(message = "Creation date cannot be in the future")
+    private LocalDateTime creationDate;
 
-    public String getContent() {return content;}
+    @PastOrPresent(message = "Last update date cannot be in the future")
+    private LocalDateTime lastUpdateDate;
 
-    public void setContent(String content) {this.content = content;}
+    @FutureOrPresent(message = "Publication date cannot be in the past")
+    private LocalDateTime publicationDate;
 
-    public Long getStatus_id() {return status_id;}
+    // Getters and Setters
 
-    public void setStatus_id(Long status_id) {this.status_id = status_id;}
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getStatusId() {
+        return statusId;
+    }
+
+    public void setStatusId(Long statusId) {
+        this.statusId = statusId;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+
+    public int getDislikes() {
+        return dislikes;
+    }
+
+    public void setDislikes(int dislikes) {
+        this.dislikes = dislikes;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public LocalDateTime getLastUpdateDate() {
+        return lastUpdateDate;
+    }
+
+    public void setLastUpdateDate(LocalDateTime lastUpdateDate) {
+        this.lastUpdateDate = lastUpdateDate;
+    }
+
+    public LocalDateTime getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(LocalDateTime publicationDate) {
+        this.publicationDate = publicationDate;
+    }
 }
