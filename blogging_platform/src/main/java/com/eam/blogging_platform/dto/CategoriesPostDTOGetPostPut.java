95369489940a494a2e71ae6bb01a1aa37bb3c5c4
Andres <arperdomo26@gmail.com>
2024-10-12 01:03:54 -0500

    feat(CategoriesPostDTOGetPostPut): is create CategoriesPostDTOGetPostPut

---
+++ blogging_platform/src/main/java/com/eam/blogging_platform/dto/CategoriesPostDTOGetPostPut.java
@@ -0,0 +1,49 @@
+package com.eam.blogging_platform.dto;
+
+import com.eam.blogging_platform.entity.CategoriesPost;
+import jakarta.validation.constraints.*;
+
+public class CategoriesPostDTOGetPostPut {
+
+    @Min(1)
+    private long id;
+
+    @Min(1)
+    private long postId;
+
+    @Min(1)
+    private long categoryId;
+
+    // Getters and Setters
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getPostId() {
+        return postId;
+    }
+
+    public void setPostId(long postId) {
+        this.postId = postId;
+    }
+
+    public long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    // Method to convert from CategoriesPost entity to CategoriesPostDTO
+    public void convertToCategoriesPostDTO(CategoriesPost categoriesPostEntity) {
+        this.setId(categoriesPostEntity.getId());
+        this.setPostId(categoriesPostEntity.getPost().getId());
+        this.setCategoryId(categoriesPostEntity.getCategory().getId());
+    }
+}
+
