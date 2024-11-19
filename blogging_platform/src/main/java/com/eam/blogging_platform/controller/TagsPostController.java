2d6ac9ab796b81c0e9336322febf2202a15b5ce2
sebastiancruzcardona <sebastiancruzcardona90@gmail.com>
2024-11-18 23:00:29 -0500

    Fix(TagsPostController): Update controller by deleting update method

--- blogging_platform/src/main/java/com/eam/blogging_platform/controller/TagsPostController.java
+++ blogging_platform/src/main/java/com/eam/blogging_platform/controller/TagsPostController.java
@@ -49,15 +49,6 @@ public class TagsPostController {
         return saveTagPost.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    @PutMapping("/{id}")
-    //This method calls the update method from tagsPostService that needs an id and a Tag_PostDto object and returns an Optional
-    //Then, tries to map the Optional tag_postDtoGetPostPut by using the .ok() function from ResponseEntity, for this the tag_postDtoGetPostPut has to be present
-    //If the optional is empty, executes the orElseGet() implementing a ResponseEntity.notFound().build()
-    public ResponseEntity<Tag_PostDtoGetPostPut> updateTagPost(@PathVariable long id, @Valid @RequestBody Tag_PostDto tagPostDto) {
-        Optional<Tag_PostDtoGetPostPut> tag_postDtoGetPostPut = tagsPostService.update(id, tagPostDto);
-        return tag_postDtoGetPostPut.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
-    }
-
     //This method refers to tagsPostService.findById() and tagsPostService.deleteById() methods. Finds a specific tagspost searching by id and deletes it
     //If the tag_post is found, deletes it.
     //If there is not a tag_post identified by that id, returns 404 Not Found Status
